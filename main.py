8fcebd4924335b14d7067d3b2bfe459b807acd57
heatforge <heattheforge@gmail.com>
2025-07-18 12:35:14 +0200

    Added sample json request object and polished off easy prompting API.

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-from openai import OpenAI 
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+jsonObject :dict = json.load(open("sample-request.json"))
+openRouterApiKey = os.getenv("OPENROUTER_API_KEY")
 
 client = OpenAI(
-    
+    api_key=openRouterApiKey,
+    base_url="https://openrouter.ai/api/v1/",
 )
+
+completion = client.chat.completions.create(
+    model="mistralai/mistral-nemo",
+    messages=[
+        {"role": "assistant", "content": "You are a helpful animal fact dispenser."},
+        {
+            "role": "user",
+            "content": "Give me random animal fun fact.",
+        },
+    ],
+)
+
+
+print(completion.choices[0].message.content)
